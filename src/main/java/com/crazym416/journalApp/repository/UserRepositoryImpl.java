14f17edca28055d444f25ca4d31013f5f9ae9cb5
ashishnamdeo <ashishnamdev14@gmail.com>
2025-12-17 11:10:29 -0800

    Implemented EmailService

---
+++ src/main/java/com/crazym416/journalApp/repository/UserRepositoryImpl.java
@@ -0,0 +1,26 @@
+package com.crazym416.journalApp.repository;
+
+import com.crazym416.journalApp.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class UserRepositoryImpl {
+
+    @Autowired
+    private MongoTemplate mongotemplate;
+
+    public List<User> getUserForSA(){
+        Query query = new Query();
+        query.addCriteria(Criteria.where("email").regex("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$"));
+        query.addCriteria(Criteria.where("sentimentAnalysis").is(true));
+        List<User> users = mongotemplate.find(query,User.class);
+        return users;
+    }
+
+}
---
+++ src/test/java/com/crazym416/journalApp/EmailServiceTests.java
@@ -0,0 +1,23 @@
+package com.crazym416.journalApp;
+
+import com.crazym416.journalApp.service.EmailService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EmailServiceTests {
+
+    @Autowired
+    private EmailService emailService;
+
+    @Test
+    void testSendMail() {
+        System.out.println("testSendMail triggered!");
+        emailService.sendEmail(
+                "ashish.namdeo.528@my.csun.edu",
+                "Checking Brother",
+                "Just chill"
+        );
+    }
+}
